8ee9a1ea7daa3c1643ac7b7e74645accee27d58d
Aman Aswal <amanpotato11@gmail.com>
2021-06-10 18:35:02 +0530

    integrated razorpay

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -8,26 +8,91 @@ const baseUrl = `http://localhost:8000`;
 function App() {
 
   const [products, setProducts] = useState([]);
+  // to verify/confirm that this is a genuine payment
+  const [payment, setPayment] = useState(false);
+  const [orderId, setOrderId] = useState('');
+  const [paymentId, setPaymentId] = useState('');
+  const [signature, setSignature] = useState('');
 
-  useEffect(()=> {
+  useEffect(() => {
     getProducts();
-  },[]);
+  }, []);
 
   const getProducts = async () => {
     const res = await axios.get(`${baseUrl}/products`);
     console.log(res);
-    if(res.status === 200){
+    if (res.status === 200) {
       setProducts(res.data);
     }
   };
 
-  const buyNow = (productId) => {
-    alert(productId);
+  const buyNow = async (productId) => {
+    const res = await axios.get(`${baseUrl}/order/${productId}`);
+    console.log(res);
+
+    if(res.status !== 200){
+      return;
+    }
+
+    const options = {
+      "key": "rzp_test_eEejfbbfnunXC1", // Enter the Key ID generated from the Dashboard
+      "amount": res.data.amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      "currency": res.data.currency,
+      "name": "Coding Warriors",
+      "description": res.data.notes.desc,
+      "image": "https://example.com/your_logo",
+      "order_id": res.data.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+      "handler": function (response) {
+        // alert(response.razorpay_payment_id);
+        // alert(response.razorpay_order_id);
+        // alert(response.razorpay_signature)
+        setOrderId(response.razorpay_order_id);
+        setPaymentId(response.razorpay_payment_id);
+        setSignature(response.razorpay_signature);
+        setPayment(true);
+      },
+      "prefill": {
+        "name": "Customer",
+        "email": "customer@example.com",
+        "contact": "9999999999"
+      },
+      // "notes": {
+      //   "address": "Razorpay Corporate Office"
+      // },
+      // "theme": {
+      //   "color": "#3399cc"
+      // }
+    };
+    const rzp1 = new window.Razorpay(options);
+
+    rzp1.open();
+
+    rzp1.on('payment.failed', function (response) {
+      alert(response.error.code);
+      alert(response.error.description);
+      alert(response.error.source);
+      alert(response.error.step);
+      alert(response.error.reason);
+      alert(response.error.metadata.order_id);
+      alert(response.error.metadata.payment_id);
+    });
   }
 
   return (
     <div className="App">
       <h1>welcome</h1>
+      {/* <header className="App-header">
+        <div>
+          {payment && (
+            <div>
+              <p>Payment Id: {paymentId}</p>
+              <p>Order Id: {orderId}</p>
+              <p>Razorpay Signature: {signature}</p>
+            </div>
+          )}
+        </div>
+      </header> */}
+
       <Products products={products} buyNow={buyNow} />
     </div>
   );
--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,42 @@
 const express = require('express');
 const cors = require('cors');
-const razorpay = require('razorpay');
+const Razorpay = require('razorpay');
 
 const { products } = require('./data');
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-app.get('/products', (req, res) =>{
+// razorpay instance
+const key_id = 'rzp_test_eEejfbbfnunXC1';
+const key_secret = '6PJjWXdL2PRZ0Drrr5Kkgmcp';
+const instance = new Razorpay({ 
+    key_id: key_id, 
+    key_secret: key_secret 
+});
+
+app.get('/products', (req, res) => {
     res.status(200).json(products);
 })
 
-app.listen(8000, ()=>{
+app.get('/order/:productId', (req, res) => {
+    const { productId } = req.params;
+    const product = products.find(product => product.id == productId);
+    const amount = product.price * 100 * 70;
+    const currency = "INR";
+    const receipt = 'receipt';
+    const notes = { desc : product.description };
+
+    instance.orders.create({amount, currency, receipt, notes}, (error, order) => {
+        if(error){
+            return res.status(500).json(error);
+        }
+        return res.status(200).json(order);
+    })
+})
+
+app.listen(8000, () => {
     console.log('Server is listening on port' + 8000);
 })
